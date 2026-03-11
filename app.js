63a9573bb64e8674db09b5f3f1aaa89f678a4918
Dmytro Sovbianov <d.sovbianov@foodtech.team>
2026-03-11 12:56:04 +0200

    add buldspec.yml

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Elastic Beanstalk via CodePipeline!\n');
+  res.end('Hello from Elastic Beanstalk via CodePipeline.v1!\n');
 });
 
 server.listen(port, () => {
